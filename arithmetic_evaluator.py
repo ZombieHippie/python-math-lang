18aeddfeeb427a5dac0b6cfdf2511368cb20bb56
CheezitBandit <geiger117@live.missouristate.edu>
2016-03-05 15:58:59 -0600

    Work in Progress
    Here's what I've done so far, if you want to check over my logic and see
    if what I've done makes any sense. If you have any suggestions, please
    let me know. I'll be back later to work on it more.

--- arithmetic_evaluator.py
+++ arithmetic_evaluator.py
@@ -24,17 +24,74 @@ def evaluate(ast):
 		return 86
 	else:
 		# demonstration code
-		if (type(ast) is dict):
-			operator = ast['operator']
-			left = ast['L']
-			right = ast['R']
-			# probably make use of a recursive strategy
-			# may be useful: https://chris-lamb.co.uk/posts/visitor-pattern-in-python
-			return -1
+		evaluateTree(ast);
 
-		elif (type(ast) is float or type(ast) is int):
-			# ast is already a number
-			return ast
-
+def evaluateTree(ast):
+	if (type(ast) is dict):
+		operator = ast['operator']
+		left = ast['L']
+		right = ast['R']
+		int i = 0;
+		
+		#When left half of operation is dictionary:
+		if (type(left) is dict):
+			#Remove the left branch and evaluateTree(left)
+			#when down to ints and operators, proceed
+			for 'L', 'operator', 'R' in left.iteritems():
+				if 'operator' == ADD:
+					return left['L'] + left['R']
+				elif 'operator' == SUBTRACT:
+					return left['L'] - left['R']
+				elif 'operator' == MULTIPLY:
+					return left['L'] * left['R']
+				elif 'operator' == DIVIDE:
+					return left['L'] / left['R']
+				else:
+					raise TypeError()
+			#Return evaluated left half of the tree
+		
+		#When left is summed but right is dictionary:
+		elif (type(right) is dict):
+			#Remove the right branch and evaluateTree(right)
+			#when down to ints and operators, proceed
+			for 'L', 'operator', 'R' in right.iteritems():
+				if 'operator' == ADD:
+					return right['L'] + right['R']
+					return right['R']
+				elif 'operator' == SUBTRACT:
+					return right['L'] - right['R']
+				elif 'operator' == MULTIPLY:
+					return right['L'] * right['R']
+				elif 'operator' == DIVIDE:
+					return right['L'] / right['R']
+				else:
+					raise TypeError()
+			#Return evaluated right half of tree
+			
+		#When L and R are summed ints w operator between:
 		else:
-			raise	TypeError()
+			for 'L', 'operator', 'R' in ast.iteritems():
+				if 'operator' == ADD:
+					return ast['L'] + ast['R']
+				elif 'operator' == SUBTRACT:
+					return ast['L'] - ast['R']
+				elif 'operator' == MULTIPLY:
+					return ast['L'] * ast['R']
+				elif 'operator' == DIVIDE:
+					return ast['L'] / ast['R']
+				else:
+					raise TypeError()
+		
+		
+			
+
+	elif (type(ast) is float or type(ast) is int):
+		# ast is already a number
+		return ast
+
+	else:
+		raise TypeError()
+		
+		
+
+
