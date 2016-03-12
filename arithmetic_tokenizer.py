14d40d92c35bfc6a8ad3aba41a9e056a735583dc
Jacob Addotta <jacobaddotta@gmail.com>
2016-03-12 11:56:14 -0600

    not done but I think its close

--- arithmetic_tokenizer.py
+++ arithmetic_tokenizer.py
@@ -3,6 +3,11 @@ from operators import *
 
 # Takes a source string and returns an array of tokens
 def tokenize(str):
+
+	token_map = {'+':'ADD', '-':'SUBTRACT',
+             '*':'MULTIPLY', '/':'DIVIDE',
+             '(':'LPAR', ')':'RPAR'}
+
 	if (str == "4 * 5 + 1"):
 		# return hardcoded answer to first test
 		return [
@@ -14,7 +19,15 @@ def tokenize(str):
 		]
 	else:
 		# demonstration code
+		# res.append({ 'type': 'number', 'value': 3 })
+		# res.append({ 'type': 'operator', 'value': ADD })
 		res = []
-		res.append({ 'type': 'number', 'value': 3 })
-		res.append({ 'type': 'operator', 'value': ADD })
+
+		split_expr = str.findall('[\d.]+|[%s]' % ''.join(token_map), expr)
+		for x in split_expr:
+			if (isinstance(elements[n], str)):
+				res.append = ({ 'type': 'operator', 'value': (token_map.get(x)) })
+			elif (isinstance(elements[n], int)):
+				res.append = ({ 'type': 'number', 'value': x })
+		
 		return res
