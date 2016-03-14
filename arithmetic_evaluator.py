d8aae024d6d4492e646a73e474b326bc94b13d80
Cole R Lawrence <cole@zombiehippie.com>
2016-03-13 20:53:12 -0500

    Working with the test files

--- arithmetic_evaluator.py
+++ arithmetic_evaluator.py
@@ -3,32 +3,12 @@ from operators import *
 
 # Testing github comprehension
 # Takes a source string and returns a parse tree
-ast = input('Enter your equation: ')
 
 def evaluate(ast):
-        if (ast == {
-                        'operator': MULTIPLY,
-                        'L': 4,
-                        'R': {
-                                'operator': ADD,
-                                'L': 5,
-                                'R': 1
-                        }
-                }):
-                # return hardcoded answer to first test
-                return 21
-        elif (ast == {
-                        'operator': SUBTRACT,
-                        'L': { 'operator': SUBTRACT, 'L': 90, 'R': -1 },
-                        'R': 5
-                }):
-                # return hardcoded answer to first test
-                return 86
-        else:
-                # demonstration code
-                recursive(ast);
+        # demonstration code
+        return recursive(ast);
 
-def recursive(ast, L, R):
+def recursive(ast):
 
         if (type(ast) is float):
                 return ast
@@ -38,64 +18,22 @@ def recursive(ast, L, R):
                 
         elif (type(ast) is dict):
                 operator = ast['operator']
-                left = ast['L']
-                right = ast['R']
+                left = recursive(ast['L'])
+                right = recursive(ast['R'])
                 print(ast)
                 
                 #When left half of operation is dictionary:
-                if (type(left) is dict):
-                        # Delve into left side, preserve original right side
-                        if left['operator'] == ADD:
-                                ast['L'] = (left['L'] + left['R'])
-                                print(ast['L'])
-                        elif left['operator'] == SUBTRACT:
-                                ast['L'] = (left['L'] - left['R'])
-                                print(ast['L'])
-                        elif left['operator'] == MULTIPLY:
-                                ast['L'] = (left['L'] + left['R'])
-                                print(ast['L'])
-                        elif left['operator'] == DIVIDE:
-                                ast['L'] = (left['L'] + left['R'])
-                                print(ast['L'])
-                        else:
-                                raise TypeError()
-                        recursive(ast, left, right)
-                        
-                #When left is summed but right is dictionary:
-                elif (type(right) is dict):
-                        # Delve into right side, preserve left
-                        if right['operator'] == ADD:
-                                ast['R'] = (right['L'] + right['R'])
-                                print(ast['R'])
-                        elif right['operator'] == SUBTRACT:
-                                ast['R'] = (right['L'] - right['R'])
-                                print(ast['R'])
-                        elif right['operator'] == MULTIPLY:
-                                ast['R'] = (right['L'] * right['R'])
-                                print(ast['R'])
-                        elif right['operator'] == DIVIDE:
-                                ast['R'] = (right['L'] / right['R'])
-                                print(ast['R'])
-                        else:
-                                raise TypeError()
-                        recursive(ast, left, right)
-                        
-                #When L and R are summed ints w operator between:
+                # Delve into left side, preserve original right side
+                if operator == ADD:
+                        return left + right
+                elif operator == SUBTRACT:
+                        return left - right
+                elif operator == MULTIPLY:
+                        return left * right
+                elif operator == DIVIDE:
+                        return left / right
                 else:
-                        if operator == ADD:
-                                print(left + right)
-                                return left + right
-                        elif operator == SUBTRACT:
-                                print(left - right)
-                                return left - right
-                        elif operator == MULTIPLY:
-                                print(left * right)
-                                return left * right
-                        elif operator == DIVIDE:
-                                print(left / right)
-                                return left / right
-                        else:
-                                raise TypeError()
+                        raise TypeError()
 
         else:
                 raise TypeError()
