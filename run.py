b9ea845271b494d6086b9e207500ec2baac5562d
Cole R Lawrence <cole@zombiehippie.com>
2016-03-14 13:19:51 -0500

    Improved input feedback for user, prettyprint, run.py

--- run.py
+++ run.py
@@ -6,17 +6,20 @@ import re
 
 import pprint
 
-pp = pprint.PrettyPrinter(indent=4)
+pp = pprint.PrettyPrinter(indent=4, width=1)
 
-valid_input_re = re.compile('^(((\d+(\.\d+)?)|([\+\-\/\*]))[ $])+$')
+re_ops = '([\+\-\/\*])'
+re_num = '(\d+(\.\d+)?)'
+valid_input_re = re.compile('^((' + re_num + '|' + re_ops + ')( |$))+$')
 
 def run():
-	print("Use CTRL+C to exit") 
+	print('')
 	try:
 		source = input('Source:')
 		is_source_valid = valid_input_re.match(source)
-		if is_source_valid:
-			print("Source string was invalid, please input an expression that is separated by single spaces")
+		is_valid_matched = is_source_valid != None
+		if not is_valid_matched:
+			print("Source string was invalid, please input an expression that is numbers and operators separated by single spaces")
 			return run()
 	except:
 		print("Error reading input")
@@ -28,10 +31,16 @@ def run():
 	print("TREE (Abstract Syntax Tree) (arithmetic_parser.py)")
 	tree = parse(tokens)
 	pp.pprint(tree)
+
+	if tree is False:
+		print("Error: Invalid source string")
+		return run()
+
 	print("VALUE (arithmetic_evaluator.py)")
 	value = evaluate(tree)
 
 	print(value)
 	run()
 
+print("Use CTRL+C to exit") 
 run()
